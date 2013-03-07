587b23d7266939a64ece5727f655345b020a9479
Joe Hare <joseph.hare@gmail.com>
2013-03-07 09:50:57 -0600

    Update .gitinit.sh

--- .gitinit.sh
+++ .gitinit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git config --global user.name "Joe Hare"
+git config --global user.name "jhare"
 git config --global user.email "joseph.hare@gmail.com"
 git config --global core.excludesfile ~/.gitignore_global 
