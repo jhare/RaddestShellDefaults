3bff2328f01152748365558b48a5205855dcf5ce
jhare <joseph.hare@gmail.com>
2013-03-07 09:52:07 -0600

    Changing the core editor to vim. It works!

--- .gitinit.sh
+++ .gitinit.sh
@@ -3,3 +3,4 @@
 git config --global user.name "Joe Hare"
 git config --global user.email "joseph.hare@gmail.com"
 git config --global core.excludesfile ~/.gitignore_global 
+git config --global core.editor "vim"
