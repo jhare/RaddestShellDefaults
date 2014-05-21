2144109140ba98cab929992d50dea71bc0921f35
Joe Hare <joseph.hare@gmail.com>
2014-05-21 11:16:53 -0500

    Setting up neobundle line

--- default-programs.sh
+++ default-programs.sh
@@ -4,3 +4,6 @@ sudo apt-get upgrade
 sudo apt-get install git mercurial build-essential awesome awesome-extras
 sudo apt-get install openssh-server vim-gtk conky htop tig 
 sudo apt-get install terminator nmap wget curl pidgin irssi
+
+# Install NeoBundle for vim
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
