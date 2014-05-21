2294f8ad4dc1ee5828a695a40dca633eb945c89c
Joe Hare <joseph.hare@gmail.com>
2014-05-21 15:38:47 -0500

    Adding some needed libraries

--- default-programs.sh
+++ default-programs.sh
@@ -4,6 +4,7 @@ sudo apt-get upgrade
 sudo apt-get install git mercurial build-essential awesome awesome-extras
 sudo apt-get install openssh-server vim-gtk conky htop tig 
 sudo apt-get install terminator nmap wget curl pidgin irssi
+sudo apt-get install libncurses5 libncurses5-dev liblua5.1-dev python2.7-dev
 
 # Install NeoBundle for vim
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
